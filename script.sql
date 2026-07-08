d1b34f6645f2b109d34619659241f2261aee8492
Salvador Antunes <salvadorantunes06@gmail.com>
2026-07-08 22:10:39 +0100

    minor changes

--- script.sql
+++ script.sql
@@ -1,7 +1,7 @@
 DROP TABLE users CASCADE constraints;
 
 CREATE TABLE users(
-    id number(9,0) primary key,
+    id number(9,0)  DEFAULT idGenerator.NEXTVAL primary key,
     username varchar2(15) unique,
     email varchar2(254) unique,
     uName varchar2(15),
@@ -12,28 +12,29 @@ CREATE TABLE users(
 alter table users add constraint min_age check(FLOOR(MONTHS_BETWEEN(SYSDATE, dateOfBirth) / 12) >= 13);
 
 create table posts(
-    id number(9,0) primary key,
-    user number(9,0),
-    postDate date,
-    text varchar2(250)
-);
-
-create table comments(
-    id number(9,0) primary key,
-    postCommented number(9,0),
+    id number(9,0) DEFAULT idGenerator.NEXTVAL primary key,
     user number(9,0),
     postDate date,
     text varchar2(250)
 );
 
 alter table posts add constraint fk_posts_user foreign key (user) references users(id);
-alter table posts add constraint fk_comments_user foreign key (user) references users(id);
+
+create table comments(
+    id number(9,0) primary key,
+    postCommented number(9,0)
+);
+
+alter table comments add constraint fk_comment foreign key (id) references posts(id);
 alter table comments add constraint fk_comment foreign key (postCommented) references posts(id);
 
 create table likes(
     userID number(9,0) primary key,
     postID number(9,0) primary key
 );
+
+alter table likes add constraint fk_user_like foreign key (userID) references users(id);
+alter table likes add constraint fk_post_like foreign key (postID) references posts(id);
 
 create or replace function likeNumber(id number) return number
 is
